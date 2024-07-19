c0889f2359098f3dffb55100f46b97dcf63fe3cc
zfr9527 <qhu15zhang3294197@163.com>
2024-07-19 16:58:24 +0800

    fix bug

---
+++ regression-test/suites/auth_call/test_cluster_backend_auth.groovy
@@ -0,0 +1,39 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+import org.junit.Assert;
+
+suite("test_cluster_backend_auth","p0,auth") {
+    String user = 'test_ddl_row_policy_auth_user'
+    String pwd = 'C123_567p'
+
+    try_sql("DROP USER ${user}")
+
+    connect(user=user, password="${pwd}", url=context.config.jdbcUrl) {
+        test {
+            sql """SHOW BACKENDS"""
+            exception "denied"
+        }
+    }
+    sql """grant node_priv on *.*.* to ${user}"""
+    connect(user=user, password="${pwd}", url=context.config.jdbcUrl) {
+        def res = sql """SHOW BACKENDS"""
+        assertTrue(res.size() > 0)
+    }
+
+    try_sql("DROP USER ${user}")
+}
