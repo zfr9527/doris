ee3960042425453d748d921f684944dc6abc34b3
zfr9527 <qhu15zhang3294197@163.com>
2024-07-19 17:49:19 +0800

    fix bug

--- regression-test/suites/auth_call/test_dml_stream_load_auth.groovy
+++ regression-test/suites/auth_call/test_dml_stream_load_auth.groovy
@@ -85,6 +85,6 @@ suite("test_dml_stream_load_auth","p0,auth") {
     logger.info("std out: " + sout + "std err: " + serr)
     assertTrue(sout.toString().indexOf("Success") != -1)
 
-    sql """drop database if exists ${dbName}"""
-    try_sql("DROP USER ${user}")
+//    sql """drop database if exists ${dbName}"""
+//    try_sql("DROP USER ${user}")
 }
