a53858b8a7f83a712cb91fadc721510a06abc91b
zfr9527 <qhu15zhang3294197@163.com>
2024-07-24 13:12:14 +0800

    fix bug

---
+++ regression-test/suites/auth_call/test_account_management_user_auth.groovy
@@ -0,0 +1,66 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+import org.junit.Assert;
+import org.codehaus.groovy.runtime.IOGroovyMethods
+
+suite("test_account_management_user_auth","p0,auth") {
+
+    String user = 'test_account_management_user_auth_user'
+    String pwd = 'C123_567p'
+    String dbName = 'test_account_management_user_auth_db'
+    String user_derive = 'test_account_management_role_user_derive_role'
+
+    try_sql("DROP USER ${user}")
+    try_sql("DROP USER ${user_derive}")
+    try_sql """drop database if exists ${dbName}"""
+
+    sql """CREATE USER '${user}' IDENTIFIED BY '${pwd}'"""
+    sql """grant select_priv on regression_test to ${user}"""
+    sql """create database ${dbName}"""
+
+    connect(user=user, password="${pwd}", url=context.config.jdbcUrl) {
+        test {
+            sql """CREATE USER ${user_derive} IDENTIFIED BY '${pwd}';"""
+            exception "denied"
+        }
+        test {
+            sql """ALTER USER ${user_derive} IDENTIFIED BY "${pwd}";"""
+            exception "denied"
+        }
+        test {
+            sql """SET PASSWORD = PASSWORD('${pwd}')"""
+            exception "denied"
+        }
+        test {
+            sql """DROP user ${user_derive}"""
+            exception "denied"
+        }
+    }
+    sql """grant grant_priv on *.*.* to ROLE '${role}'"""
+    connect(user=user, password="${pwd}", url=context.config.jdbcUrl) {
+        sql """CREATE USER ${user_derive} IDENTIFIED BY '${pwd}';"""
+        sql """ALTER USER ${user_derive} IDENTIFIED BY "${pwd}";"""
+        sql """SET PASSWORD = PASSWORD('${pwd}')"""
+        sql """DROP user ${user_derive}"""
+    }
+
+    sql """drop database if exists ${dbName}"""
+    try_sql("DROP USER ${user}")
+    try_sql("DROP role ${role}")
+    try_sql("DROP role ${role_derive}")
+}
