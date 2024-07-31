b931229124393ac0a266d690295b4bae4e50ddf8
zfr9527 <qhu15zhang3294197@163.com>
2024-07-31 19:32:42 +0800

    fix bug

--- regression-test/suites/mv_p0/base_dimension/dimension_2_4_mv.groovy
+++ regression-test/suites/mv_p0/base_dimension/dimension_2_4_mv.groovy
@@ -19,7 +19,7 @@
 This suite is a two dimensional test case file.
 It mainly tests the query partial, view partial, union rewriting, predicate compensate, project rewriting.
  */
-suite("partition_mv_rewrite_dimension_2_4_mv") {
+suite("partition_mv_rewrite_dimension_2_4_mv", "partition_mv_rewrite_dimension_2_4_mv") {
     String db = context.config.getDbNameByFile(context.file)
     sql "use ${db}"
 
