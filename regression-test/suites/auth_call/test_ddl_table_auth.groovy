c27851c797753b5d06f02a46a1283cf59fe45110
zfr9527 <qhu15zhang3294197@163.com>
2024-08-13 01:25:36 +0800

    fix bug

--- regression-test/suites/auth_call/test_ddl_table_auth.groovy
+++ regression-test/suites/auth_call/test_ddl_table_auth.groovy
@@ -94,6 +94,8 @@ suite("test_ddl_table_auth","p0,auth") {
         def db_res = sql """show tables;"""
         assertTrue(db_res.size() == 1)
 
+        sql """show query stats;"""
+
 //        def col_res = sql """SHOW FULL COLUMNS FROM ${dbName}.${tableName};"""
 //        logger.info("col_res: " + col_res)
 //        assertTrue(col_res.size() == 2)
