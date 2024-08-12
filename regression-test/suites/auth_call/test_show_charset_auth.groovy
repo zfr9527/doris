3d453031c3a4a96f13e73b6559b4b62b689d2bbd
zfr9527 <qhu15zhang3294197@163.com>
2024-08-12 17:38:39 +0800

    fix bug

--- regression-test/suites/auth_call/test_show_charset_auth.groovy
+++ regression-test/suites/auth_call/test_show_charset_auth.groovy
@@ -47,8 +47,12 @@ suite("test_show_no_auth","p0,auth") {
     connect(user=user, password="${pwd}", url=context.config.jdbcUrl) {
         def res = sql """show PROPERTY for ${user1}"""
         logger.info("res: " + res)
+
+//        def res1 = sql """SHOW PROCESSLIST"""
+//        logger.info("res1: " + res1)
+//        assertTrue(res1.size() == 1)
     }
 
-//    try_sql("DROP USER ${user}")
-//    try_sql("DROP USER ${user1}")
+    try_sql("DROP USER ${user}")
+    try_sql("DROP USER ${user1}")
 }
