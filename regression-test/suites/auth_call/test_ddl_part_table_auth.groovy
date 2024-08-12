a1406647993b8b6ab3e33a3a7e68227a7f119a97
zfr9527 <qhu15zhang3294197@163.com>
2024-08-12 14:13:48 +0800

    fix bug

--- regression-test/suites/auth_call/test_ddl_part_table_auth.groovy
+++ regression-test/suites/auth_call/test_ddl_part_table_auth.groovy
@@ -17,17 +17,18 @@
 
 import org.junit.Assert;
 
-suite("test_show_dynamic_table_auth","p0,auth") {
-    String user = 'test_show_dynamic_table_auth_user'
+suite("test_ddl_part_table_auth","p0,auth") {
+    String user = 'test_ddl_part_table_auth_user'
     String pwd = 'C123_567p'
-    String dbName = 'test_show_dynamic_table_auth_db'
-    String tableName = 'test_show_dynamic_table_auth_tb'
+    String dbName = 'test_ddl_part_table_auth_db'
+    String tableName = 'test_ddl_part_table_auth_tb'
 
     try_sql("DROP USER ${user}")
     try_sql """drop database if exists ${dbName}"""
     sql """CREATE USER '${user}' IDENTIFIED BY '${pwd}'"""
     sql """grant select_priv on regression_test to ${user}"""
     sql """create database ${dbName}"""
+
     sql """create table ${dbName}.${tableName} (
                 id BIGINT,
                 username VARCHAR(20)
@@ -35,24 +36,15 @@ suite("test_show_dynamic_table_auth","p0,auth") {
             PARTITION BY RANGE(id) ()
             DISTRIBUTED BY HASH(id) BUCKETS 2
             PROPERTIES (
-                "replication_num" = "1",
-                "dynamic_partition.enable" = "true",
-                "dynamic_partition.time_unit" = "MONTH",
-                "dynamic_partition.end" = "2",
-                "dynamic_partition.prefix" = "p",
-                "dynamic_partition.buckets" = "8",
-                "dynamic_partition.start_day_of_month" = "3"
+                "replication_num" = "1"
             );"""
 
-    connect(user=user, password="${pwd}", url=context.config.jdbcUrl) {
-        def res = sql """SHOW DYNAMIC PARTITION TABLES from ${dbName};"""
-        assertTrue(res.size() == 0)
-    }
-    sql """grant select_priv on ${dbName}.${tableName} to ${user}"""
-    connect(user=user, password="${pwd}", url=context.config.jdbcUrl) {
-        def res = sql """SHOW DYNAMIC PARTITION TABLES from ${dbName};"""
-        assertTrue(res.size() == 1)
-    }
+    sql """alter table ${dbName}.${tableName} add partition p1 VALUES [("1"), ("2"));"""
+    sql """insert into ${dbName}.${tableName} values (1, "111");"""
+
+
+
+
 
 //    sql """drop database if exists ${dbName}"""
 //    try_sql("DROP USER ${user}")
