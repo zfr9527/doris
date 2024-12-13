11662cb67caed8b3de54d9012d0f00553c36f2dc
zhangfurong <zhangfurong@selectdb.com>
2024-12-13 14:29:01 +0800

    fix bug

--- regression-test/suites/auth_random_up_down_p0/test_random_grant_revoke_auth.groovy
+++ regression-test/suites/auth_random_up_down_p0/test_random_grant_revoke_auth.groovy
@@ -91,14 +91,17 @@ suite("test_random_upgrade_downgrade_compatibility_auth","p0,auth,restart_fe") {
         sql """revoke SHOW_VIEW_PRIV on ${dbName} from ${user1}"""
     }
 
-    /*
+
     connect(user1, "${pwd}", context.config.jdbcUrl) {
         def grants_res = sql """show grants;"""
         if (!version_no_1) {
             assertTrue(grants_res[0][4] == """internal.default_cluster:information_schema: Select_priv  (false); internal.default_cluster:regression_test: Select_priv  (false)""")
             assertTrue(grants_res[0][5] == null)
         } else {
-
+            logger.info("grants_res[0][5]: |" + grants_res[0][5] + "|")
+            logger.info("grants_res[0][6]: |" + grants_res[0][6] + "|")
+            assertTrue(grants_res[0][5] == "internal.default_cluster:information_schema: Select_priv ; internal.default_cluster:mysql: Select_priv ; internal.default_cluster:regression_test: Select_priv ")
+            assertTrue(grants_res[0][6] == null)
         }
 
     }
@@ -128,34 +131,46 @@ suite("test_random_upgrade_downgrade_compatibility_auth","p0,auth,restart_fe") {
 
         connect(user1, "${pwd}", context.config.jdbcUrl) {
             def grants_res = sql """show grants;"""
-            assertTrue(grants_res[0][3] == """default_cluster:test_random_up_down_auth_role1""")
-            assertTrue(grants_res[0][4] == """internal.default_cluster:information_schema: Select_priv ; internal.default_cluster:mysql: Select_priv ; internal.default_cluster:regression_test: Select_priv ; internal.default_cluster:test_auth_up_down_db: Select_priv Load_priv Alter_priv Create_priv Drop_priv Show_view_priv""")
-            assertTrue(grants_res[0][5] == """internal.default_cluster:test_auth_up_down_db.test_auth_up_down_table1: Select_priv Load_priv Alter_priv Create_priv Drop_priv Show_view_priv ; test_auth_up_down_db.default_cluster:test_auth_up_down_table1.id: Select_priv Load_priv Alter_priv Create_priv Drop_priv Show_view_priv""")
+            assertTrue(grants_res[0][2] == """default_cluster:test_random_up_down_auth_role1 """)
+            assertTrue(grants_res[0][5] == "internal.default_cluster:information_schema: Select_priv ; internal.default_cluster:mysql: Select_priv ; internal.default_cluster:regression_test: Select_priv ; internal.default_cluster:test_auth_up_down_db: Select_priv Load_priv Alter_priv Create_priv Drop_priv Show_view_priv ")
+            assertTrue(grants_res[0][6] == "internal.default_cluster:test_auth_up_down_db.test_auth_up_down_table1: Select_priv Load_priv Alter_priv Create_priv Drop_priv Show_view_priv ; test_auth_up_down_db.default_cluster:test_auth_up_down_table1.id: Select_priv Load_priv Alter_priv Create_priv Drop_priv Show_view_priv ")
         }
 
-        sql """grant select_priv on ${dbName}.${tableName1}.id to ROLE '${role1}'"""
-        sql """grant select_priv on ${dbName}.${tableName1} to ROLE '${role1}'"""
-        sql """grant select_priv on ${dbName} to ROLE '${role1}'"""
-        sql """grant LOAD_PRIV on ${dbName}.${tableName1}.id to ROLE '${role1}'"""
-        sql """grant LOAD_PRIV on ${dbName}.${tableName1} to ROLE '${role1}'"""
-        sql """grant LOAD_PRIV on ${dbName} to ROLE '${role1}'"""
-        sql """grant ALTER_PRIV on ${dbName}.${tableName1}.id to ROLE '${role1}'"""
-        sql """grant ALTER_PRIV on ${dbName}.${tableName1} to ROLE '${role1}'"""
-        sql """grant ALTER_PRIV on ${dbName} to ROLE '${role1}'"""
-        sql """grant CREATE_PRIV on ${dbName}.${tableName1}.id to ROLE '${role1}'"""
-        sql """grant CREATE_PRIV on ${dbName}.${tableName1} to ROLE '${role1}'"""
-        sql """grant CREATE_PRIV on ${dbName} to ROLE '${role1}'"""
-        sql """grant DROP_PRIV on ${dbName}.${tableName1}.id to ROLE '${role1}'"""
-        sql """grant DROP_PRIV on ${dbName}.${tableName1} to ROLE '${role1}'"""
-        sql """grant DROP_PRIV on ${dbName} to ROLE '${role1}'"""
-        sql """grant SHOW_VIEW_PRIV on ${dbName}.${tableName1}.id to ROLE '${role1}'"""
-        sql """grant SHOW_VIEW_PRIV on ${dbName}.${tableName1} to ROLE '${role1}'"""
-        sql """grant SHOW_VIEW_PRIV on ${dbName} to ROLE '${role1}'"""
+        sql """revoke '${role1}' from '${user1}'"""
+        connect(user1, "${pwd}", context.config.jdbcUrl) {
+            def grants_res = sql """show grants;"""
+            assertTrue(grants_res[0][2] == """default_cluster:test_random_up_down_auth_role1 """)
+            assertTrue(grants_res[0][5] == "internal.default_cluster:information_schema: Select_priv ; internal.default_cluster:mysql: Select_priv ; internal.default_cluster:regression_test: Select_priv ; internal.default_cluster:test_auth_up_down_db: Select_priv Load_priv Alter_priv Create_priv Drop_priv Show_view_priv ")
+            assertTrue(grants_res[0][6] == "internal.default_cluster:test_auth_up_down_db.test_auth_up_down_table1: Select_priv Load_priv Alter_priv Create_priv Drop_priv Show_view_priv ; test_auth_up_down_db.default_cluster:test_auth_up_down_table1.id: Select_priv Load_priv Alter_priv Create_priv Drop_priv Show_view_priv ")
+        }
+
+        sql """grant '${role1}' to '${user1}'"""
+        sql """revoke select_priv on ${dbName}.${tableName1}.id from ROLE '${role1}'"""
+        sql """revoke select_priv on ${dbName}.${tableName1} from ROLE '${role1}'"""
+        sql """revoke select_priv on ${dbName} from ROLE '${role1}'"""
+        sql """revoke LOAD_PRIV on ${dbName}.${tableName1}.id from ROLE '${role1}'"""
+        sql """revoke LOAD_PRIV on ${dbName}.${tableName1} from ROLE '${role1}'"""
+        sql """revoke LOAD_PRIV on ${dbName} from ROLE '${role1}'"""
+        sql """revoke ALTER_PRIV on ${dbName}.${tableName1}.id from ROLE '${role1}'"""
+        sql """revoke ALTER_PRIV on ${dbName}.${tableName1} from ROLE '${role1}'"""
+        sql """revoke ALTER_PRIV on ${dbName} from ROLE '${role1}'"""
+        sql """revoke CREATE_PRIV on ${dbName}.${tableName1}.id from ROLE '${role1}'"""
+        sql """revoke CREATE_PRIV on ${dbName}.${tableName1} from ROLE '${role1}'"""
+        sql """revoke CREATE_PRIV on ${dbName} from ROLE '${role1}'"""
+        sql """revoke DROP_PRIV on ${dbName}.${tableName1}.id from ROLE '${role1}'"""
+        sql """revoke DROP_PRIV on ${dbName}.${tableName1} from ROLE '${role1}'"""
+        sql """revoke DROP_PRIV on ${dbName} from ROLE '${role1}'"""
+        sql """revoke SHOW_VIEW_PRIV on ${dbName}.${tableName1}.id from ROLE '${role1}'"""
+        sql """revoke SHOW_VIEW_PRIV on ${dbName}.${tableName1} from ROLE '${role1}'"""
+        sql """revoke SHOW_VIEW_PRIV on ${dbName} from ROLE '${role1}'"""
+
+        connect(user1, "${pwd}", context.config.jdbcUrl) {
+            def grants_res = sql """show grants;"""
+            assertTrue(grants_res[0][2] == """default_cluster:test_random_up_down_auth_role1 """)
+            assertTrue(grants_res[0][5] == "internal.default_cluster:information_schema: Select_priv ; internal.default_cluster:mysql: Select_priv ; internal.default_cluster:regression_test: Select_priv ; internal.default_cluster:test_auth_up_down_db: Select_priv Load_priv Alter_priv Create_priv Drop_priv Show_view_priv ")
+            assertTrue(grants_res[0][6] == "internal.default_cluster:test_auth_up_down_db.test_auth_up_down_table1: Select_priv Load_priv Alter_priv Create_priv Drop_priv Show_view_priv ; test_auth_up_down_db.default_cluster:test_auth_up_down_table1.id: Select_priv Load_priv Alter_priv Create_priv Drop_priv Show_view_priv ")
+        }
     }
-
-
-
- */
 
 
 //    如何检测远程机器上文件是否存在
