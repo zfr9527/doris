f7e66ab7e69d39696cb7f611ed176bd7f83f8c66
zhangfurong <zhangfurong@selectdb.com>
2025-02-25 19:36:32 +0800

    fix bug

--- regression-test/suites/mtmv_up_down_olap_p0/test_upgrade_downgrade_olap_mtmv.groovy
+++ regression-test/suites/mtmv_up_down_olap_p0/test_upgrade_downgrade_olap_mtmv.groovy
@@ -81,7 +81,7 @@ suite("test_upgrade_downgrade_olap_mtmv","p0,mtmv,restart_fe") {
     // 单独刷新分区执行报错，且刷新之后分区没有被删除
     test {
         sql """refresh MATERIALIZED VIEW ${dropMtmvName2} partition(${mtmv_part_res[0][1]})"""
-        exception "partition not exist"
+        exception "partition not"
     }
 
     // 刷新整个mtmv，分区会被删除
