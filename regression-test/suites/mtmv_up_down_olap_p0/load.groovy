f2e415c5ad5aa9b517cdf147499ba41b19c907b9
zhangfurong <zhangfurong@selectdb.com>
2025-03-10 10:43:12 +0800

    fix bug

--- regression-test/suites/mtmv_up_down_olap_p0/load.groovy
+++ regression-test/suites/mtmv_up_down_olap_p0/load.groovy
@@ -35,71 +35,13 @@ corresponding rewriting meets the expectations.
 suite("test_upgrade_downgrade_prepare_olap_mtmv","p0,mtmv,restart_fe") {
     String suiteName = "mtmv_up_down_olap"
     String dbName = context.config.getDbNameByFile(context.file)
-    String mvName = "${suiteName}_mtmv"
-    String tableName = "${suiteName}_table"
-    String tableName2 = "${suiteName}_table2"
 
-    sql """drop materialized view if exists ${mvName};"""
-    sql """drop table if exists `${tableName}`"""
-    sql """drop table if exists `${tableName2}`"""
-
-    sql """
-        CREATE TABLE `${tableName}` (
-          `user_id` LARGEINT NOT NULL COMMENT '\"用户id\"',
-          `date` DATE NOT NULL COMMENT '\"数据灌入日期时间\"',
-          `num` SMALLINT NOT NULL COMMENT '\"数量\"'
-        ) ENGINE=OLAP
-        DUPLICATE KEY(`user_id`, `date`, `num`)
-        COMMENT 'OLAP'
-        PARTITION BY RANGE(`date`)
-        (PARTITION p201701_1000 VALUES [('0000-01-01'), ('2017-02-01')),
-        PARTITION p201702_2000 VALUES [('2017-02-01'), ('2017-03-01')),
-        PARTITION p201703_all VALUES [('2017-03-01'), ('2017-04-01')))
-        DISTRIBUTED BY HASH(`user_id`) BUCKETS 2
-        PROPERTIES ('replication_num' = '1') ;
-        """
-    sql """
-        insert into ${tableName} values(1,"2017-01-15",1),(2,"2017-02-15",2),(3,"2017-03-15",3);
-        """
-
-    sql """
-        CREATE TABLE `${tableName2}` (
-          `user_id` LARGEINT NOT NULL COMMENT '\"用户id\"',
-          `age` SMALLINT NOT NULL COMMENT '\"年龄\"'
-        ) ENGINE=OLAP
-        DUPLICATE KEY(`user_id`, `age`)
-        COMMENT 'OLAP'
-        DISTRIBUTED BY HASH(`user_id`) BUCKETS 2
-        PROPERTIES ('replication_num' = '1') ;
-        """
-    sql """
-        insert into ${tableName2} values(1,1),(2,2),(3,3);
-        """
-
-    sql """
-        CREATE MATERIALIZED VIEW ${mvName}
-            REFRESH AUTO ON MANUAL
-            partition by(`date`)
-            DISTRIBUTED BY RANDOM BUCKETS 2
-            PROPERTIES ('replication_num' = '1')
-            AS
-            SELECT a.* FROM ${tableName} a inner join ${tableName2} b on a.user_id=b.user_id;
-    """
-    waitingMTMVTaskFinishedByMvName(mvName)
-
-
-    String dropTableName1 = """${suiteName}_DropTableName1"""
-    String dropTableName2 = """${suiteName}_DropTableName2"""
-    String dropTableName4 = """${suiteName}_DropTableName4"""
-    String dropMtmvName1 = """${suiteName}_dropMtmvName1"""
-    String dropMtmvName2 = """${suiteName}_dropMtmvName2"""
-    String dropMtmvName3 = """${suiteName}_dropMtmvName3"""
+    String dropTableName1 = """${suiteName}_TableName1"""
+    String dropTableName4 = """${suiteName}_TableName4"""
+    String dropMtmvName1 = """${suiteName}_MtmvName1"""
 
     sql """drop materialized view if exists ${dropMtmvName1};"""
-    sql """drop materialized view if exists ${dropMtmvName2};"""
-    sql """drop materialized view if exists ${dropMtmvName3};"""
     sql """drop table if exists `${dropTableName1}`"""
-    sql """drop table if exists `${dropTableName2}`"""
     sql """drop table if exists `${dropTableName4}`"""
 
 
@@ -132,34 +74,6 @@ suite("test_upgrade_downgrade_prepare_olap_mtmv","p0,mtmv,restart_fe") {
         """
 
     sql """
-        CREATE TABLE `${dropTableName2}` (
-          `user_id` LARGEINT NOT NULL COMMENT '\"用户id\"',
-          `date` DATE NOT NULL COMMENT '\"数据灌入日期时间\"',
-          `num` SMALLINT NOT NULL COMMENT '\"数量\"'
-        ) ENGINE=OLAP
-        DUPLICATE KEY(`user_id`, `date`, `num`)
-        COMMENT 'OLAP'
-        PARTITION BY RANGE(`date`)
-        (PARTITION p201701_1000 VALUES [('0000-01-01'), ('2017-02-01')),
-        PARTITION p201702_2000 VALUES [('2017-02-01'), ('2017-03-01')),
-        PARTITION p201703_3000 VALUES [('2017-03-01'), ('2017-04-01')),
-        PARTITION p201704_4000 VALUES [('2017-04-01'), ('2017-05-01')),
-        PARTITION p201705_5000 VALUES [('2017-05-01'), ('2017-06-01')),
-        PARTITION p201706_6000 VALUES [('2017-06-01'), ('2017-07-01')),
-        PARTITION p201707_7000 VALUES [('2017-07-01'), ('2017-08-01')),
-        PARTITION p201708_8000 VALUES [('2017-08-01'), ('2017-09-01')),
-        PARTITION p201709_9000 VALUES [('2017-09-01'), ('2017-10-01')),
-        PARTITION p201710_1000 VALUES [('2017-10-01'), ('2017-11-01')),
-        PARTITION p201711_1100 VALUES [('2017-11-01'), ('2017-12-01')),
-        PARTITION p201712_1200 VALUES [('2017-12-01'), ('2018-01-01')))
-        DISTRIBUTED BY HASH(`user_id`) BUCKETS 2
-        PROPERTIES ('replication_num' = '1') ;
-        """
-    sql """
-        insert into ${dropTableName2} values(1,"2017-01-15",1),(2,"2017-02-15",2),(3,"2017-03-15",3),(4,"2017-04-15",4),(5,"2017-05-15",5),(6,"2017-06-15",6),(7,"2017-07-15",7),(8,"2017-08-15",8),(9,"2017-09-15",9),(10,"2017-10-15",10),(11,"2017-11-15",11),(12,"2017-12-15",12);
-        """
-
-    sql """
         CREATE TABLE `${dropTableName4}` (
           `user_id` LARGEINT NOT NULL COMMENT '\"用户id\"',
           `age` SMALLINT NOT NULL COMMENT '\"年龄\"'
@@ -185,24 +99,7 @@ suite("test_upgrade_downgrade_prepare_olap_mtmv","p0,mtmv,restart_fe") {
     """
     waitingMTMVTaskFinishedByMvName(dropMtmvName1)
 
-    sql """
-        CREATE MATERIALIZED VIEW ${dropMtmvName2}
-            REFRESH AUTO ON MANUAL
-            partition by(`date`)
-            DISTRIBUTED BY RANDOM BUCKETS 2
-            PROPERTIES ('replication_num' = '1')
-            AS
-            SELECT a.* FROM ${dropTableName2} a inner join ${dropTableName4} b on a.user_id=b.user_id;
-    """
-    waitingMTMVTaskFinishedByMvName(dropMtmvName2)
+    sql """drop table ${dropTableName1}"""
 
-    def state_mtmv1 = sql """select State,RefreshState,SyncWithBaseTables from mv_infos('database'='${dbName}') where Name = '${dropMtmvName1}';"""
-    assertTrue(state_mtmv1[0][0] == "NORMAL")
-    assertTrue(state_mtmv1[0][1] == "SUCCESS")
-    assertTrue(state_mtmv1[0][2] == true)
-    def state_mtmv2 = sql """select State,RefreshState,SyncWithBaseTables from mv_infos('database'='${dbName}') where Name = '${dropMtmvName2}';"""
-    assertTrue(state_mtmv2[0][0] == "NORMAL")
-    assertTrue(state_mtmv2[0][1] == "SUCCESS")
-    assertTrue(state_mtmv2[0][2] == true)
 
 }
