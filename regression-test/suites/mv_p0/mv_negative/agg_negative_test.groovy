c037184c103b8dc2f28c05647330f33b33b96834
zhangfurong <zhangfurong@selectdb.com>
2025-05-22 11:33:28 +0800

    fix bug

--- regression-test/suites/mv_p0/mv_negative/agg_negative_test.groovy
+++ regression-test/suites/mv_p0/mv_negative/agg_negative_test.groovy
@@ -1,3 +1,20 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 suite("agg_negative_mv_test") {
 
     String db = context.config.getDbNameByFile(context.file)
@@ -149,10 +166,9 @@ suite("agg_negative_mv_test") {
         exception """isKey must same with all slot"""
     }
 
-    // 这个异常信息我不理解
     test {
         sql """create materialized view ${no_mv_name} as select col3, col1, col2, col15, sum(col7), bitmap_union(to_bitmap(case when col10 > 1 then 1 else 2 end)) from ${tb_name} group by 1,2,3,4  order by  1,2,3,4"""
-        exception """only allow col10#10 as bitmap_union's param"""
+        exception """only allow single column as bitmap_union's param"""
     }
 
     sql """create materialized view ${mv_name}_1 as select col3, col1, col2, col15, sum(col7) from ${tb_name} group by 1,2,3,4  order by  1,2,3,4"""
