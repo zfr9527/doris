66bbd47be82e1067c9506b7b4e00ddd25350cab5
zhangfurong <zhangfurong@selectdb.com>
2025-09-17 11:26:55 +0800

    fix bug

--- regression-test/suites/nereids_rules_p0/mv/pre_rewrite/mtmv_rbo_pre_rewrite.groovy
+++ regression-test/suites/nereids_rules_p0/mv/pre_rewrite/mtmv_rbo_pre_rewrite.groovy
@@ -1,4 +1,4 @@
-package mv.pre_rewrite.limit
+package mv.pre_rewrite
 // Licensed to the Apache Software Foundation (ASF) under one
 // or more contributor license agreements.  See the NOTICE file
 // distributed with this work for additional information
