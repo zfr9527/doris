949a3393ac5a421e48c25d87447ad23024262491
zhangfurong <zhangfurong@selectdb.com>
2025-10-16 17:41:47 +0800

    fix bug

--- regression-test/suites/query_p0/cache/query_cache_with_mtmv.groovy
+++ regression-test/suites/query_p0/cache/query_cache_with_mtmv.groovy
@@ -707,8 +707,8 @@ suite("query_cache_with_mtmv") {
                 create_async_mv(dbName, mv_name1, mtmv_sql)
                 assertHasCache select_sql // 直查表，不改写mtmv1
                 assertNoCache mtmv_select_sql  // 直查表，改写mtmv1
-                assertNoCache nested_mtmv_select_sql1 // 直查mtmv1，改写nested_mtmv1(嵌套物化不可用，直查物化，而且物化又是才被创建，怎么会命中缓存呢？)
                 assertNoCache nested_mtmv_select_sql2 // 直查表，改写nested_mtmv1
+                assertHasCache nested_mtmv_select_sql1 // 直查mtmv1，改写nested_mtmv1(嵌套物化不可用，直查物化，而且物化又是才被创建，怎么会命中缓存呢？)
                 assertNoCache nested_mtmv_select_sql3 // 直查mtmv1，不改写nested_mtmv1
                 assertHasCache nested_mtmv_select_sql // 直查nested_mtmv1，不改写
 
