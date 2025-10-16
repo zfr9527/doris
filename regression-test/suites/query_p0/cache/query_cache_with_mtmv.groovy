d27c7b8bb66c53d86265952337ab874a03d3f9a6
zhangfurong <zhangfurong@selectdb.com>
2025-10-16 17:19:07 +0800

    fix bug

--- regression-test/suites/query_p0/cache/query_cache_with_mtmv.groovy
+++ regression-test/suites/query_p0/cache/query_cache_with_mtmv.groovy
@@ -682,37 +682,13 @@ suite("query_cache_with_mtmv") {
                 create_async_mv(dbName, mv_name2, mtmv_sql2)
                 create_async_mv(dbName, nested_mv_name1, nested_mtmv_sql)
 
-                assertNoCache select_sql // 直查表，不改写mtmv1
-                assertNoCache mtmv_select_sql  // 直查表，改写mtmv1
-                assertNoCache nested_mtmv_select_sql2 // 直查表，改写nested_mtmv1
-                assertNoCache nested_mtmv_select_sql1 // 直查mtmv1，改写nested_mtmv1
-                assertNoCache nested_mtmv_select_sql3 // 直查mtmv1，不改写nested_mtmv1
-                assertNoCache nested_mtmv_select_sql // 直查nested_mtmv1，不改写
 
-                assertHasCache select_sql // 直查表，不改写mtmv1
-                assertHasCache mtmv_select_sql  // 直查表，改写mtmv1
-                assertHasCache nested_mtmv_select_sql2 // 直查表，改写nested_mtmv1
+                assertNoCache nested_mtmv_select_sql1 // 直查mtmv1，改写nested_mtmv1
                 assertHasCache nested_mtmv_select_sql1 // 直查mtmv1，改写nested_mtmv1
-                assertHasCache nested_mtmv_select_sql3 // 直查mtmv1，不改写nested_mtmv1
-                assertHasCache nested_mtmv_select_sql // 直查nested_mtmv1，不改写
-
                 sql """DROP MATERIALIZED VIEW IF EXISTS ${mv_name1}"""
-                assertHasCache select_sql // 直查表，不改写mtmv1
-                assertNoCache mtmv_select_sql  // 直查表，改写mtmv1
-                assertNoCache nested_mtmv_select_sql2 // 直查表，改写nested_mtmv1
-                test {
-                    sql nested_mtmv_select_sql1
-                    exception "does not exist"
-                }
-                assertHasCache nested_mtmv_select_sql // 直查nested_mtmv1，不改写
-
                 create_async_mv(dbName, mv_name1, mtmv_sql)
-                assertHasCache select_sql // 直查表，不改写mtmv1
-                assertNoCache mtmv_select_sql  // 直查表，改写mtmv1
-                assertNoCache nested_mtmv_select_sql2 // 直查表，改写nested_mtmv1
-                assertNoCache nested_mtmv_select_sql1 // 直查mtmv1，改写nested_mtmv1
-                assertNoCache nested_mtmv_select_sql3 // 直查mtmv1，不改写nested_mtmv1
-                assertHasCache nested_mtmv_select_sql // 直查nested_mtmv1，不改写
+                assertNoCache nested_mtmv_select_sql1 // 直查mtmv1，改写nested_mtmv1(嵌套物化不可用，直查物化，而且物化又是才被创建，怎么会命中缓存呢？)
+
 
             }),
 
