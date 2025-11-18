c33af0556ae3e84b442325b63e5d07ec09347c01
zhangfurong <zhangfurong@selectdb.com>
2025-11-18 14:47:42 +0800

    fix bug

--- regression-test/suites/variables_up_down3/load.groovy
+++ regression-test/suites/variables_up_down3/load.groovy
@@ -134,6 +134,11 @@ suite("variables_up_down_load3") {
         PROPERTIES("replication_num" = "1");
     """
     sql "set enable_decimal256=false;"
+    test {
+        sql "insert into t_gen_col_if values(1.12343,1.123457,default);"
+        exception "Not Supported"
+    }
+    sql "set enable_decimal256=true;"
     sql "insert into t_gen_col_if values(1.12343,1.123457,default);"
     qt_gen_col_if "select * from t_gen_col_if;"
 
