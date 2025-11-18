2ad325124aa70e83d33fa9a217da6f3b68e0cc4f
zhangfurong <zhangfurong@selectdb.com>
2025-11-18 15:09:58 +0800

    fix bug

--- regression-test/suites/variables_up_down5/load.groovy
+++ regression-test/suites/variables_up_down5/load.groovy
@@ -5,6 +5,32 @@ suite("variables_up_down_load5") {
     // 这个是截断到38个精度的结果
     // 999999999999998246906000000000.76833464
 
+    multi_sql """
+        drop table if exists test_decimal_mul_overflow1;
+        CREATE TABLE `test_decimal_mul_overflow1` (
+            `f1` decimal(20,5) NULL,
+            `f2` decimal(21,6) NULL
+        )DISTRIBUTED BY HASH(f1)
+        PROPERTIES("replication_num" = "1");
+        insert into test_decimal_mul_overflow1 values(999999999999999.12345,999999999999999.123456);
+    """
+
+    multi_sql """
+        drop table if exists t_decimalv3;
+        create table t_decimalv3(a decimal(38,9),b decimal(38,10))
+        DISTRIBUTED BY HASH(a)
+        PROPERTIES("replication_num" = "1");
+        insert into t_decimalv3 values(1.012345678,1.0123456789);
+    """
+
+    multi_sql """
+        drop table if exists t_decimalv3_for_compare;
+        create table t_decimalv3_for_compare(a decimal(38,9),b decimal(38,10))
+        DISTRIBUTED BY HASH(a)
+        PROPERTIES("replication_num" = "1");
+        insert into t_decimalv3_for_compare values(1.012345678,1.0123456781);
+    """
+
     // 打开enable256,创建视图;
     sql "set enable_decimal256=true;"
     sql "drop view if exists v_test_decimal_mul_overflow1;"
