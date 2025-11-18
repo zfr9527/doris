064bab52e2eb8d11b1f71471e0c72022f38c5173
zhangfurong <zhangfurong@selectdb.com>
2025-11-18 15:32:32 +0800

    fix bug

--- regression-test/suites/variables_up_down6/load.groovy
+++ regression-test/suites/variables_up_down6/load.groovy
@@ -23,12 +23,12 @@ suite("variables_up_down_load6") {
              """
     */
 
-    sql "set enable_decimal256=false;"
+    sql "set enable_decimal256=true;"
     sql """ DROP TABLE IF EXISTS t01; """
     sql """
         create table t01(id int, col_sum agg_state<sum(decimalv3(76,6))> generic, col_avg agg_state<avg(decimalv3(76,6))> generic)  properties ("replication_num" = "1");
         """
-
+    sql "set enable_decimal256=false;"
     sql """
     insert into t01 values (1, sum_state(10.1), avg_state(10.1)), (1, sum_state(20.1), avg_state(20.1)), (2, sum_state(10.2), avg_state(10.2)), (2, sum_state(11.0), avg_state(11.0));
     """
