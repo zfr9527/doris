339ecc9d71e5a0487f28db613d199248d88bdca3
zhangfurong <zhangfurong@selectdb.com>
2025-11-24 17:34:33 +0800

    fix bug

--- regression-test/suites/ldap_p0/modify_ldap_group_and_member.groovy
+++ regression-test/suites/ldap_p0/modify_ldap_group_and_member.groovy
@@ -25,7 +25,7 @@ suite("modify_ldap_group_and_member", "external_docker") {
     String prefix_str = "z10100_"
     String dbName = prefix_str + "db"
     String tbName = prefix_str + "tb"
-//    String tbName2 = prefix_str + "_tb2"
+    String tbName2 = prefix_str + "_tb2"
     sql """create database if not exists ${dbName}"""
     sql """drop table if exists ${dbName}.${tbName}"""
     sql """create table ${dbName}.${tbName} (
@@ -43,21 +43,21 @@ suite("modify_ldap_group_and_member", "external_docker") {
         (3, "333");
         """
 
-//    sql """drop table if exists ${dbName}.${tbName2}"""
-//    sql """create table ${dbName}.${tbName2} (
-//                id BIGINT,
-//                username VARCHAR(20)
-//            )
-//            DISTRIBUTED BY HASH(id) BUCKETS 2
-//            PROPERTIES (
-//                "replication_num" = "1"
-//            );"""
-//    sql """
-//        insert into ${dbName}.`${tbName2}` values
-//        (1, "111"),
-//        (2, "222"),
-//        (3, "333");
-//        """
+    sql """drop table if exists ${dbName}.${tbName2}"""
+    sql """create table ${dbName}.${tbName2} (
+                id BIGINT,
+                username VARCHAR(20)
+            )
+            DISTRIBUTED BY HASH(id) BUCKETS 2
+            PROPERTIES (
+                "replication_num" = "1"
+            );"""
+    sql """
+        insert into ${dbName}.`${tbName2}` values
+        (1, "111"),
+        (2, "222"),
+        (3, "333");
+        """
 
     // Get LDAP connection details from config
     String ldapHost = context.config.otherConfigs.get("ldapHost")
@@ -76,17 +76,13 @@ suite("modify_ldap_group_and_member", "external_docker") {
     String testUserPlaintextPassword = "654321"
     
     String testUserDn = "uid=${testUser},cn=${testGroup},${ldapBaseDn}"
-//    String testUserDn2 = "uid=${testUser},cn=${testGroup2},${ldapBaseDn}"
+    String testUserDn2 = "uid=${testUser},cn=${testGroup2},${ldapBaseDn}"
     String testGroupDn = "cn=${testGroup},${ldapBaseDn}"
-//    String testGroupDn2 = "cn=${testGroup2},${ldapBaseDn}"
+    String testGroupDn2 = "cn=${testGroup2},${ldapBaseDn}"
 
-    for (String dn in [testUserDn, testGroupDn]) {
+    for (String dn in [testUserDn, testUserDn2, testGroupDn, testGroupDn2]) {
         deleteLdapEntry("""ldap://${ldapHost}:${ldapPort}""", ldapAdminUser, ldapAdminPassword, dn)
     }
-
-//    for (String dn in [testUserDn, testUserDn2, testGroupDn, testGroupDn2]) {
-//        deleteLdapEntry("""ldap://${ldapHost}:${ldapPort}""", ldapAdminUser, ldapAdminPassword, dn)
-//    }
     
     // Prepare the multi-entry LDIF file content
     String ldifContent = """dn: cn=${testGroup},${ldapBaseDn}
@@ -112,12 +108,11 @@ suite("modify_ldap_group_and_member", "external_docker") {
     sql "CREATE ROLE '${testGroup}';"
     sql "GRANT SELECT_PRIV ON ${dbName}.${tbName} TO ROLE '${testGroup}';" // Grant some privilege to the role
 
-//    sql """drop role if exists ${testGroup2}"""
-//    sql "CREATE ROLE '${testGroup2}';"
-//    sql "GRANT SELECT_PRIV ON ${dbName}.${tbName2} TO ROLE '${testGroup2}';" // Grant some privilege to the role
+    sql """drop role if exists ${testGroup2}"""
+    sql "CREATE ROLE '${testGroup2}';"
+    sql "GRANT SELECT_PRIV ON ${dbName}.${tbName2} TO ROLE '${testGroup2}';" // Grant some privilege to the role
 
-//    logger.info("Successfully created role '${testGroup}', '${testGroup2}' in Doris.")
-    logger.info("Successfully created role '${testGroup}' in Doris.")
+    logger.info("Successfully created role '${testGroup}', '${testGroup2}' in Doris.")
 
     // Step 3: Verify that the new user can log in and has the correct role's permissions
     def tokens = context.config.jdbcUrl.split('/')
@@ -152,59 +147,34 @@ suite("modify_ldap_group_and_member", "external_docker") {
             sql """select * from ${dbName}.${tbName}"""
             exception "Permission denied"
         }
+        def res = sql """select * from ${dbName}.${tbName2}"""
+        assertTrue(res.size() == 3)
     }
 
 
+    deleteLdapEntry("""ldap://${ldapHost}:${ldapPort}""", ldapAdminUser, ldapAdminPassword, testGroupDn2)
+    sql """REFRESH LDAP FOR ${testUser};"""
+    connect(testUser, testUserPlaintextPassword, url) {
+        logger.info("SUCCESS: LDAP user '${testUser}' successfully logged in to Doris.")
+        def grants = sql """show grants;"""
+        logger.info("grants: " + grants)
+        assertFalse(grants.toString().contains("${testGroup}"))
+        test {
+            sql """select * from ${dbName}.${tbName}"""
+            exception "Permission denied"
+        }
+        test {
+            sql """select * from ${dbName}.${tbName2}"""
+            exception "Permission denied"
+        }
+    }
 
-
-//    // uid from group1 to group2
-//    String moveLdifContent = """dn: uid=${testUser},cn=${testGroup},${ldapBaseDn}
-//        changetype: modrdn
-//        newrdn: uid=${testUser}
-//        deleteoldrdn: 1
-//        newsuperior: cn=${testGroup2},${ldapBaseDn}"""
-//    moveLdapEntry("""ldap://${ldapHost}:${ldapPort}""", ldapAdminUser, ldapAdminPassword, testUserDn, testGroupDn2, moveLdifContent)
-//    sql """REFRESH LDAP FOR ${testUser};"""
-//    connect(testUser, testUserPlaintextPassword, url) {
-//        def grants = sql """show grants;"""
-//        logger.info("grants: " + grants)
-//        assertTrue(grants.toString().contains("${testGroup}"))
-//        assertFalse(grants.toString().contains("${testGroup2}"))
-//        def res = sql """select * from ${dbName}.${tbName}"""
-//        assertTrue(res.size() == 3)
-//        logger.info("SUCCESS: LDAP user '${testUser}' successfully logged in to Doris.")
-//    }
-//
-//    assertTrue(deleteMemberToEntry("""ldap://${ldapHost}:${ldapPort}""", ldapAdminUser, ldapAdminPassword, testGroupDn, testUserDn2))
-//    sql """REFRESH LDAP FOR ${testUser};"""
-//    connect(testUser, testUserPlaintextPassword, url) {
-//        def grants = sql """show grants;"""
-//        logger.info("grants: " + grants)
-//        assertFalse(grants.toString().contains("${testGroup}"))
-//        assertFalse(grants.toString().contains("${testGroup2}"))
-//        logger.info("SUCCESS: LDAP user '${testUser}' successfully logged in to Doris.")
-//    }
-//
-//    assertTrue(addMemberToEntry("""ldap://${ldapHost}:${ldapPort}""", ldapAdminUser, ldapAdminPassword, testGroupDn2, testUserDn2))
-//    sql """REFRESH LDAP FOR ${testUser};"""
-//    connect(testUser, testUserPlaintextPassword, url) {
-//        def grants = sql """show grants;"""
-//        logger.info("grants: " + grants)
-//        assertFalse(grants.toString().contains("${testGroup}"))
-//        assertTrue(grants.toString().contains("${testGroup2}"))
-//        def res = sql """select * from ${dbName}.${tbName2}"""
-//        assertTrue(res.size() == 3)
-//        logger.info("SUCCESS: LDAP user '${testUser}' successfully logged in to Doris.")
-//    }
-//
-//    assertFalse(checkLdapEntryExist("""ldap://${ldapHost}:${ldapPort}""", ldapAdminUser, ldapAdminPassword, testUserDn))
-//    assertTrue(checkLdapEntryExist("""ldap://${ldapHost}:${ldapPort}""", ldapAdminUser, ldapAdminPassword, testUserDn2))
-    
     // Clean up: always try to remove all created entities
     logger.info("Starting cleanup process...")
     sql "DROP ROLE '${testGroup}';"
+    sql "DROP ROLE '${testGroup2}';"
 
-    for (String dn in [testUserDn, testGroupDn]) {
+    for (String dn in [testUserDn, testUserDn2, testGroupDn, testGroupDn2]) {
         deleteLdapEntry("""ldap://${ldapHost}:${ldapPort}""", ldapAdminUser, ldapAdminPassword, dn)
     }
 
